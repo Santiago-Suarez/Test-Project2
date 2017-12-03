ea928df2683e23b9e4a1c12cf658d5c3416785cb
Santiago-Suarez <34010204+Santiago-Suarez@users.noreply.github.com>
2017-12-03 22:17:23 +0100

    Create Buen-dia.py

---
+++ Buen-dia.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+respuesta = raw_input("¿Hace un buen día hoy?:" )
+
+(respuesta.lower())
+
+if respuesta == "si" or "sí":
+    print "Me alegro"
+
+else:
+    print "Vaya, espero que se ponga mejor"
